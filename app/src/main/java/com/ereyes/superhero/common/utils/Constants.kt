a3752f0d53abe56814afbd3f8407996aeb5dd46a
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-02-11 16:05:08 -0600

    object
    Se agrega object Constants para todas la const que pudieran ocuparse

---
+++ app/src/main/java/com/ereyes/superhero/common/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.ereyes.superhero.common.utils
+
+/****
+ * Project: SuperHero
+ * From: com.ereyes.superhero.common.utils
+ * Created by Edgar Reyes Gonzalez on 2/11/2023 at 1:27 PM
+ * All rights reserved 2023.
+ ****/
+object Constants {
+    const val BASE_URL = "https://superheroapi.com"
+}
